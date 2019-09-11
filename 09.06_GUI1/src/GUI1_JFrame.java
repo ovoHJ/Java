94e03ce3bf5d0c91eba00140a8d90cdf6e278483
ovoHJ <s2018w01@gmail.com>
2019-09-11 09:08:42 +0900

    Sample

--- 09.06_GUI1/src/GUI1_JFrame.java
+++ 09.06_GUI1/src/GUI1_JFrame.java
@@ -1,14 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
-        JLabel label = new JLabel("아싸 어쩌다 금요일");
+        Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
+        int 화w = res.width;
+        int 화h = res.height;
+        int 창w = 400;
+        int 창h = 200;
+        frame.setPreferredSize(new Dimension(창w, 창h));
 
-        frame.add(label);
+        frame.setLocation((화w-창w)/2, (화h-창h)/2);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("아싸 어쩌다 금요일");
+        JButton button = new JButton("오예");
+
+        panel.add(label);
+        panel.add(button);
+        frame.add(panel);
 
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+   }
 }
