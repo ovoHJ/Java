bc3e7e9f26cae2d12f366fa7c4a8b16645896c68
ovoHJ <s2018w01@gmail.com>
2019-11-01 08:45:57 +0900

    주민번호테스터 GUI, UX

--- 09.06_GUI1/src/Sample.java
+++ 09.06_GUI1/src/Sample.java
@@ -8,7 +8,7 @@ public class Sample {
 
         frame.add(panel);
 
-        frame.setPreferredSize(new Dimension(600, 400));
+        frame.setPreferredSize(new Dimension(800, 1000));
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
